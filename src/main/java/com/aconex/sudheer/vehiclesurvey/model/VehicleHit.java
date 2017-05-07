0e96d97fd07f3e5339715e02e7e446babe3cee80
Sudheer Palyam <palyam.sudheer@gmail.com>
2017-05-07 23:44:47 +0530

    TDD - Test Driven Implementation - Test Case to Validate VehicleHit Pojo

--- src/main/java/com/aconex/sudheer/vehiclesurvey/model/VehicleHit.java
+++ src/main/java/com/aconex/sudheer/vehiclesurvey/model/VehicleHit.java
@@ -15,55 +15,73 @@ public class VehicleHit {
 	char bound;
 	String timeSinceMidNight;
 	int day;
-	
+
 	public int getDay() {
 		return day;
 	}
+
 	public void setDay(int day) {
 		this.day = day;
 	}
+
 	public String getData() {
 		return data;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
+
 	public long getRecordedTimeMills() {
 		return recordedTimeMills;
 	}
+
 	public void setRecordedTimeMills(long recordedTimeMills) {
 		this.recordedTimeMills = recordedTimeMills;
 	}
+
 	public long getEpochTimeMills() {
 		return epochTimeMills;
 	}
+
 	public void setEpochTimeMills(long epochTimeMills) {
+		if (epochTimeMills <= 0) {
+		      throw new IllegalArgumentException("Vehicle Hit Time cannot be Negative or Zero");
+		    }
 		this.epochTimeMills = epochTimeMills;
 	}
+
 	public char getBound() {
 		return bound;
 	}
+
 	public void setBound(char bound) {
 		this.bound = bound;
 	}
+
 	public String getTimeSinceMidNight() {
 		return timeSinceMidNight;
 	}
+
 	public void setTimeSinceMidNight(String timeSinceMidNight) {
 		this.timeSinceMidNight = timeSinceMidNight;
 	}
+
 	@Override
-	public String toString() {
-		
-		
-		return "VehicleHit [data=" + data + ", recordedTimeMills=" + recordedTimeMills + ", epochTimeMills="
-				+ epochTimeMills + ", epochTime="
-				+ TimeUtil.df.format(Instant.ofEpochMilli(epochTimeMills)) + 
-				", day=" + day + ", bound=" + bound + ", timeSinceMidNight=" + timeSinceMidNight + "]";
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (!(obj instanceof VehicleHit))
+			return false;
+		VehicleHit vh = (VehicleHit) obj;
+		return vh.getData().equals(this.getData());
 	}
 
-	
-	
-	
+	@Override
+	public String toString() {
 
+		return "VehicleHit [data=" + data + ", recordedTimeMills=" + recordedTimeMills + ", epochTimeMills=" + epochTimeMills + ", epochTime="
+				+ TimeUtil.df.format(Instant.ofEpochMilli(epochTimeMills)) + ", day=" + day + ", bound=" + bound + ", timeSinceMidNight=" + timeSinceMidNight
+				+ "]";
+	}
 }
